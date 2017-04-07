aaf26b408b7925f4703bff46e6648a4b77c10b52
papv <papv@ics.forth.gr>
2017-04-07 11:57:59 +0300

    Evaluation module constants added

--- src/main/java/org/hobbit/benchmark/versioning/properties/VersioningConstants.java
+++ src/main/java/org/hobbit/benchmark/versioning/properties/VersioningConstants.java
@@ -11,39 +11,61 @@ public final class VersioningConstants {
 	
 	// =============== DATA GENERATOR CONSTANTS ===============
 
-	public static final String DATA_GENERATOR_SEED = "DATA_GENERATOR_SEED";
+	public static final String DATA_GENERATOR_SEED = "data-generator_seed";
 
-	public static final String DATA_GENERATOR_WORKERS = "DATA_GENERATOR_WORKERS";
+	public static final String DATA_GENERATOR_WORKERS = "data-generator_workers";
 
-	public static final String NUMBER_OF_DATA_GENERATORS= "NUMBER_OF_DATA_GENERATORS";
+	public static final String NUMBER_OF_DATA_GENERATORS= "data-generators_number";
 		
 	// =============== TASK GENERATOR CONSTANTS ===============
 
-	public static final String NUMBER_OF_TASK_GENERATORS= "NUMBER_OF_TASK_GENERATORS";
+	public static final String NUMBER_OF_TASK_GENERATORS= "task-generators_number";
 
-	public static final String SUBSTITUTION_PARAMETERS_AMOUNT = "SUBSTITUTION_PARAMETERS_AMOUNT";
+	public static final String SUBSTITUTION_PARAMETERS_AMOUNT = "substitution-parameters_amount";
 
-	// =============== BENCHMARK CONSTANTS ===============
+	// =============== DATA GENERATORS CONSTANTS ===============
 
-	public static final String DATASET_SIZE_IN_TRIPLES = "DATASET_SIZE_IN_TRIPLES";
+	public static final String DATASET_SIZE_IN_TRIPLES = "dataset_size_in_triples";
 
-	public static final String GENERATED_DATA_FORMAT = "GENERATED_DATA_FORMAT";
+	public static final String GENERATED_DATA_FORMAT = "generated_data_format";
 	
-	public static final String NUMBER_OF_VERSIONS = "NUMBER_OF_VERSIONS";
+	public static final String NUMBER_OF_VERSIONS = "number_of_versions";
 	
-	public static final String GENERATED_DATA_DIR = "GENERATED_DATA_DIR";
+	public static final String GENERATED_DATA_DIR = "generated_data_dir";
 	
-	public static final String CREATIVE_WORKS_INFO = "CREATIVE_WORKS_INFO";
+	public static final String CREATIVE_WORKS_INFO = "creative_works_info";
 	
-	public static final String MAX_GENERATED_TRIPLES_PER_FILE = "MAX_GENERATED_TRIPLES_PER_FILE";
+	public static final String MAX_GENERATED_TRIPLES_PER_FILE = "max_generated_triples_per_file";
 	
-	public static final String SEED_YEAR = "SEED_YEAR";
+	public static final String SEED_YEAR = "seed_year";
 	
-	public static final String GENERATION_PERIOD_IN_YEARS = "GENERATION_PERIOD_IN_YEARS";
+	public static final String GENERATION_PERIOD_IN_YEARS = "generation_period_in_years";
 	
-	public static final String ARCHIVING_STRATEGY = "ARCHIVING_STRATEGY";
+	public static final String ARCHIVING_STRATEGY = "archiving_strategy";
 	
-	// =============== VIRTUOSO GOLD STANDARD CONSTANTS ===============
+	// =============== EVALUATION MODULE CONSTANTS ===============
+	
+	public static final String INITIAL_VERSION_INGESTION_SPEED = "initial-version_ingestion_speed";
+	
+	public static final String AVG_APPLIED_CHANGES_PS = "avg_applied_changes_ps";
+	
+	public static final String STORAGE_COST = "storage_cost";
+	
+	public static final String QT_1_AVG_EXEC_TIME = "query-type-1_avgerage_execution_time";
 
-	public static final String VIRTUOSO_GS_IMAGE_NAME = "VIRTUOSO_GS_IMAGE_NAME";
+	public static final String QT_2_AVG_EXEC_TIME = "query-type-2_avgerage_execution_time";
+
+	public static final String QT_3_AVG_EXEC_TIME = "query-type-3_avgerage_execution_time";
+
+	public static final String QT_4_AVG_EXEC_TIME = "query-type-4_avgerage_execution_time";
+
+	public static final String QT_5_AVG_EXEC_TIME = "query-type-5_avgerage_execution_time";
+
+	public static final String QT_6_AVG_EXEC_TIME = "query-type-6_avgerage_execution_time";
+
+	public static final String QT_7_AVG_EXEC_TIME = "query-type-7_avgerage_execution_time";
+
+	public static final String QT_8_AVG_EXEC_TIME = "query-type-8_avgerage_execution_time";
+
+
 }
