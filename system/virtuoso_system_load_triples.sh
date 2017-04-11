e5ce9b3721b72ffb6d375b238cc160fec3734899
papv <papv@ics.forth.gr>
2017-04-11 17:33:38 +0300

    enhance script to return the actual loading time

--- system/virtuoso_system_load_triples.sh
+++ system/virtuoso_system_load_triples.sh
@@ -40,7 +40,6 @@ total_cores=$(cat /proc/cpuinfo | grep processor | wc -l)
 rdf_loaders=$(awk "BEGIN {printf \"%d\", $total_cores/2.5}")
 
 start_load=$(($(date +%s%N)/1000000))
-
 $VIRTUOSO_BIN/isql-v 1111 dba dba exec="delete from load_list;" > /dev/null
 $VIRTUOSO_BIN/isql-v 1111 dba dba exec="sparql clear GRAPH <$GRAPH_NAME$VERSION_NUMBER>;" > /dev/null
 
@@ -62,16 +61,17 @@ wait
 
 $VIRTUOSO_BIN/isql-v 1111 dba dba exec="checkpoint;" > /dev/null
 end_load=$(($(date +%s%N)/1000000))
+loadingtime=$(($end_load - $start_load))
 
 # get the total size of loaded triples
 start_size=$(($(date +%s%N)/1000000))
 result=$($VIRTUOSO_BIN/isql-v 1111 dba dba exec="sparql select count(*) from <$GRAPH_NAME$VERSION_NUMBER> where { ?s ?p ?o };" | sed -n 9p)
 $VIRTUOSO_BIN/isql-v 1111 dba dba exec="checkpoint;" > /dev/null
 end_size=$(($(date +%s%N)/1000000))
-
-loadingtime=$(($end_size - $start_load))
 sizetime=$(($end_size - $start_size))
 
-echo "loaded triples "$result
+#
+echo "triples:"$result",time:"$loadingtime
+
 echo $(echo $result | sed ':a;s/\B[0-9]\{3\}\>/,&/;ta') "triples loaded to graph <"$GRAPH_NAME$VERSION_NUMBER">, using" $rdf_loaders "rdf loaders, for version v"$VERSION_NUMBER". Time :" $loadingtime "ms"
 
