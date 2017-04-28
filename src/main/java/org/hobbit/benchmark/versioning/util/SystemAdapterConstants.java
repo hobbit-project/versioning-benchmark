d39231a89ad2ab1b716455fa8a9b3b79e1fbcd28
papv <papv@ics.forth.gr>
2017-04-28 15:47:50 +0300

    added constants for common api

---
+++ src/main/java/org/hobbit/benchmark/versioning/util/SystemAdapterConstants.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package org.hobbit.benchmark.versioning.util;
+
+/**
+ * @author papv
+ *
+ */
+public final class SystemAdapterConstants {
+	public static final byte  BULK_LOADING_DATA_FINISHED = (byte) 150;
+    public static final byte  BULK_LOAD_DATA_GEN_FINISHED = (byte) 151;
+}
