57bf8ae68f10adc2ca9d8771429af1805fb4d88a
papv <papv@ics.forth.gr>
2017-06-26 15:11:30 +0300

    update script for executing query in order to return results in json format

--- r43ples/required_files/execute_query.sh
+++ r43ples/required_files/execute_query.sh
@@ -4,4 +4,4 @@ MODE=off
 ENDPOINT=http://localhost:9998/r43ples/sparql
 QUERY=$1
 
-curl -H "Accept: application/sparql-results+xml" --data "query_rewriting=$MODE&query= $QUERY" $ENDPOINTc
+curl -H "Accept: application/json" --data "query_rewriting=$MODE&query=$QUERY" $ENDPOINT
