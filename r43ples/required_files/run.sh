f51549365fdc379860ffca671e48f2694bd21ba8
papv <papv@ics.forth.gr>
2017-07-06 14:19:22 +0300

    tdb run

--- r43ples/required_files/run.sh
+++ r43ples/required_files/run.sh
@@ -2,7 +2,9 @@
 
 # Start R43ples
 echo $(date +%H:%M:%S.%N | cut -b1-12)" : Starting R43ples..."
-java -jar r43ples.jar --config conf/r43ples.tdb.conf &     # TDB     -> 9998
+java -jar r43ples.jar --config conf/r43ples.tdb.conf &     	# TDB     -> 9998
+#java -jar r43ples.jar --config conf/r43ples.stardog.conf &	# Stardog -> 9997
+#java -jar r43ples.jar --config conf/r43ples.virtuoso.conf &	# Virtuoso -> 9996
 
 # run the system adapter
 echo $(date +%H:%M:%S.%N | cut -b1-12)" : Running the System adapter..."
