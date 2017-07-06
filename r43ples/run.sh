0da97d669606a2e48920dae516919e2724d045b3
papv <papv@ics.forth.gr>
2017-07-06 14:16:11 +0300

    move run.sh script

--- r43ples/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Start R43ples
-java -jar r43ples.jar --config conf/r43ples.tdb.conf &     # TDB     -> 9998
-
-# run the system adapter
-echo $(date +%H:%M:%S.%N | cut -b1-12)" : Running the System adapter..."
-java -cp versioning.jar org.hobbit.core.run.ComponentStarter org.hobbit.benchmark.versioning.systems.R43plesSystemAdapter
