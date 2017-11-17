8aad421d981bb3a0a3d49e956af0258783540690
papv <papv@ics.forth.gr>
2017-11-17 23:56:20 +0200

    make virtuoso memory usage adjustable to the available resources

--- system/run.sh
+++ system/run.sh
@@ -3,6 +3,16 @@
 rm virtuoso_run.log 2> /dev/null
 cp virtuoso.ini.template virtuoso.ini
 
+# adjust virtuoso memory usage settings
+free_memory=$(free | awk 'NR==2 {print $4}')
+NumberOfBuffers=$(($free_memory*660/8000))
+MaxDirtyBuffers=$(($NumberOfBuffers*3/4))
+sed -i -e "s/NumberOfBuffers          = 10000/NumberOfBuffers          = $NumberOfBuffers/g" virtuoso.ini
+sed -i -e "s/MaxDirtyBuffers          = 6000/MaxDirtyBuffers          = $MaxDirtyBuffers/g" virtuoso.ini
+echo $(date +%H:%M:%S.%N | cut -b1-12)" : Virtuoso memory usage adjusted to the available resources:"
+echo $(date +%H:%M:%S.%N | cut -b1-12)" : NumberOfBuffers="$NumberOfBuffers
+echo $(date +%H:%M:%S.%N | cut -b1-12)" : MaxDirtyBuffers="$MaxDirtyBuffers
+
 echo $(date +%H:%M:%S.%N | cut -b1-12)" : Starting OpenLink Virtuoso Universal Server..."
 virtuoso-t -f > /versioning/virtuoso_run.log 2>&1 &
 seconds_passed=0
