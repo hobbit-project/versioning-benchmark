5c90f2e9c5e1b1a43a164f9e6cf9aef0b1bc093c
papv <papv@casta-ubuntu>
2018-01-24 16:24:12 +0200

    replace all '-' chars with '_' on env variables

--- src/main/java/org/hobbit/benchmark/versioning/properties/VersioningConstants.java
+++ src/main/java/org/hobbit/benchmark/versioning/properties/VersioningConstants.java
@@ -15,9 +15,9 @@ public final class VersioningConstants {
 		
 	// =============== DATA GENERATOR CONSTANTS ===============
 
-	public static final String DATA_GENERATOR_SEED = "data-generator_seed";
+	public static final String DATA_GENERATOR_SEED = "data_generator_seed";
 
-	public static final String NUMBER_OF_DATA_GENERATORS= "data-generators_number";
+	public static final String NUMBER_OF_DATA_GENERATORS= "data_generators_number";
 	
 	public static final String V0_SIZE_IN_TRIPLES = "v0_size_in_triples";
 
@@ -31,39 +31,39 @@ public final class VersioningConstants {
 		
 	// =============== TASK GENERATOR CONSTANTS ===============
 
-	public static final String NUMBER_OF_TASK_GENERATORS= "task-generators_number";
+	public static final String NUMBER_OF_TASK_GENERATORS= "task_generators_number";
 	
 	// =============== EVALUATION MODULE CONSTANTS ===============
 	
-	public static final String INITIAL_VERSION_INGESTION_SPEED = "initial-version_ingestion_speed";
+	public static final String INITIAL_VERSION_INGESTION_SPEED = "initial_version_ingestion_speed";
 	
 	public static final String AVG_APPLIED_CHANGES_PS = "avg_applied_changes_ps";
 	
 	public static final String STORAGE_COST = "storage_cost";
 	
-	public static final String QT_1_AVG_EXEC_TIME = "query-type-1_avgerage_execution_time";
+	public static final String QT_1_AVG_EXEC_TIME = "query_type_1_avgerage_execution_time";
 
-	public static final String QT_2_AVG_EXEC_TIME = "query-type-2_avgerage_execution_time";
+	public static final String QT_2_AVG_EXEC_TIME = "query_type_2_avgerage_execution_time";
 
-	public static final String QT_3_AVG_EXEC_TIME = "query-type-3_avgerage_execution_time";
+	public static final String QT_3_AVG_EXEC_TIME = "query_type_3_avgerage_execution_time";
 
-	public static final String QT_4_AVG_EXEC_TIME = "query-type-4_avgerage_execution_time";
+	public static final String QT_4_AVG_EXEC_TIME = "query_type_4_avgerage_execution_time";
 
-	public static final String QT_5_AVG_EXEC_TIME = "query-type-5_avgerage_execution_time";
+	public static final String QT_5_AVG_EXEC_TIME = "query_type_5_avgerage_execution_time";
 
-	public static final String QT_6_AVG_EXEC_TIME = "query-type-6_avgerage_execution_time";
+	public static final String QT_6_AVG_EXEC_TIME = "query_type_6_avgerage_execution_time";
 
-	public static final String QT_7_AVG_EXEC_TIME = "query-type-7_avgerage_execution_time";
+	public static final String QT_7_AVG_EXEC_TIME = "query_type_7_avgerage_execution_time";
 
-	public static final String QT_8_AVG_EXEC_TIME = "query-type-8_avgerage_execution_time";
+	public static final String QT_8_AVG_EXEC_TIME = "query_type_8_avgerage_execution_time";
 
-	public static final String QUERY_FAILURES = "query-failures";
+	public static final String QUERY_FAILURES = "query_failures";
 	
-	public static final String QUERIES_PER_SECOND = "queries-per-second";
+	public static final String QUERIES_PER_SECOND = "queries_per_second";
 	
-	public static final String LOADING_TIMES = "version-%d_loading-time";
+	public static final String LOADING_TIMES = "version_%d_loading_time";
 	
-	public static final String TRIPLES_TO_BE_LOADED= "version-%d_triples-to-be-loaded";
+	public static final String TRIPLES_TO_BE_LOADED= "version_%d_triples_to_be_loaded";
 
 	public static final String TOTAL_VERSIONS = "versions_number";
 
