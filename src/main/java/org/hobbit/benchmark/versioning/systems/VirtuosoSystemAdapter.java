1acd14c71d9e4d570b43a5ebb70d9d1627732c1c
papv <papv@casta-ubuntu>
2018-01-26 12:01:18 +0200

    implement loading of different versions for SPBv v2.0

--- src/main/java/org/hobbit/benchmark/versioning/systems/VirtuosoSystemAdapter.java
+++ src/main/java/org/hobbit/benchmark/versioning/systems/VirtuosoSystemAdapter.java
@@ -13,9 +13,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.ByteBuffer;
 import java.util.concurrent.Semaphore;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import org.aksw.jena_sparql_api.http.QueryExecutionFactoryHttp;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.jena.query.Query;
@@ -24,7 +24,6 @@ import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.query.QueryFactory;
 import org.apache.jena.query.ResultSet;
 import org.apache.jena.query.ResultSetFormatter;
-import org.hobbit.benchmark.versioning.properties.RDFUtils;
 import org.hobbit.benchmark.versioning.util.VirtuosoSystemAdapterConstants;
 import org.hobbit.core.components.AbstractSystemAdapter;
 import org.hobbit.core.rabbit.RabbitMQUtils;
@@ -45,37 +44,30 @@ public class VirtuosoSystemAdapter extends AbstractSystemAdapter {
 
 	// used to check if bulk loading phase has finished in  order to proceed with the querying phase
 	private boolean dataLoadingFinished = false;
-	private int loadingVersion = 0;
-	
-	// must match the "Generated data format" parameter given when starting the experiment
-	private String generatedDataFormat = "n-triples";
-	long initialDatasetsSize = 0;
+	private int loadingNumber = 0;
+	private String datasetFolderName;
+	private String virtuosoContName = "localhost";
 
 	@Override
     public void init() throws Exception {
 		LOGGER.info("Initializing virtuoso test system...");
         super.init();	
+        datasetFolderName = "/versioning/data/";
+        File theDir = new File(datasetFolderName);
+		theDir.mkdir();
 		LOGGER.info("Virtuoso initialized successfully .");
     }
 
 	/* (non-Javadoc)
 	 * @see org.hobbit.core.components.TaskReceivingComponent#receiveGeneratedData(byte[])
+	 * EFTIAKSA TO RECEIVE NA KSEKINISW APO EDW AVRIO PARASKEVI
 	 */
-	public void receiveGeneratedData(byte[] data) {
-		String dataPath = "/versioning/data/";
-		
+	public void receiveGeneratedData(byte[] data) {		
 		ByteBuffer dataBuffer = ByteBuffer.wrap(data);
 		// read the graph uri in order to identify the version in which
 		// received data will be loaded into.
-		String graphUri = RabbitMQUtils.readString(dataBuffer);
-		String receivedFilePath;
-		
-		if (graphUri.startsWith("http://datagen.version.0")) {
-			receivedFilePath = dataPath + "v0/" + graphUri.replaceFirst(".*/", "");
-		} else {
-			String versionNum = graphUri.substring(25, graphUri.indexOf("generatedCreativeWorks") - 1);
-			receivedFilePath = dataPath + "c" + versionNum + "/" + graphUri.replaceFirst(".*/", "");
-		}
+		String fileName = RabbitMQUtils.readString(dataBuffer);
+
 		// read the data contents
 		byte[] dataContentBytes = new byte[dataBuffer.remaining()];
 		dataBuffer.get(dataContentBytes, 0, dataBuffer.remaining());
@@ -83,16 +75,20 @@ public class VirtuosoSystemAdapter extends AbstractSystemAdapter {
 		if (dataContentBytes.length != 0) {
 			FileOutputStream fos = null;
 			try {
-				File outputFile = new File(receivedFilePath);
-				fos = FileUtils.openOutputStream(outputFile, false);
+				if (fileName.contains("/")) {
+					fileName = fileName.replaceAll("[^/]*[/]", "");
+				}
+				fos = new FileOutputStream(datasetFolderName + File.separator + fileName);
 				IOUtils.write(dataContentBytes, fos);
 				fos.close();
+				
 				// test
-				BufferedReader reader = new BufferedReader(new FileReader(receivedFilePath));
+				BufferedReader reader = new BufferedReader(new FileReader(datasetFolderName));
 				int lines = 0;
 				while (reader.readLine() != null) lines++;
 				reader.close();
-				LOGGER.info(receivedFilePath + " (" + (double) new File(receivedFilePath).length() / 1000 + " KB) received from Data Generator with " + lines + " lines.");
+				LOGGER.info(datasetFolderName + " (" + (double) new File(datasetFolderName).length() / 1000 + " KB) received from Data Generator with " + lines + " lines.");
+				// test
 
 			} catch (FileNotFoundException e) {
 				LOGGER.error("Exception while creating/opening files to write received data.", e);
@@ -142,11 +138,11 @@ public class VirtuosoSystemAdapter extends AbstractSystemAdapter {
 		} 
 	}
 	
-	private void loadVersion(int versionNum) {
-		LOGGER.info("Loading version " + versionNum + "...");
+	private void loadVersion(String graphURI) {
+		LOGGER.info("Loading data on " + graphURI + "...");
 		try {
 			String scriptFilePath = System.getProperty("user.dir") + File.separator + "load.sh";
-			String[] command = {"/bin/bash", scriptFilePath, RDFUtils.getFileExtensionFromRdfFormat(generatedDataFormat), Integer.toString(versionNum) };
+			String[] command = {"/bin/bash", scriptFilePath, virtuosoContName, datasetFolderName, graphURI};
 			Process p = new ProcessBuilder(command).redirectErrorStream(true).start();
 			BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
 			String line;
@@ -154,7 +150,7 @@ public class VirtuosoSystemAdapter extends AbstractSystemAdapter {
 				LOGGER.info(line);		
 			}
 			p.waitFor();
-			LOGGER.info("Version " + versionNum + " loaded successfully.");
+			LOGGER.info(graphURI + " loaded successfully.");
 			in.close();
 		} catch (IOException e) {
             LOGGER.error("Exception while executing script for loading data.", e);
@@ -169,7 +165,7 @@ public class VirtuosoSystemAdapter extends AbstractSystemAdapter {
     		ByteBuffer buffer = ByteBuffer.wrap(data);
             int numberOfMessages = buffer.getInt();
             boolean lastLoadingPhase = buffer.get() != 0;
-   			LOGGER.info("Received signal that all data of version " + loadingVersion + " successfully sent from all data generators (#" + numberOfMessages + ")");
+   			LOGGER.info("Received signal that all data of version " + loadingNumber + " successfully sent from all data generators (#" + numberOfMessages + ")");
 
 			// if all data have been received before BULK_LOAD_DATA_GEN_FINISHED command received
    			// release before acquire, so it can immediately proceed to bulk loading
@@ -177,23 +173,27 @@ public class VirtuosoSystemAdapter extends AbstractSystemAdapter {
 				allVersionDataReceivedMutex.release();
 			}
 			
-			LOGGER.info("Wait for receiving all data of version " + loadingVersion + ".");
+			LOGGER.info("Wait for receiving all data of version " + loadingNumber + ".");
 			try {
 				allVersionDataReceivedMutex.acquire();
 			} catch (InterruptedException e) {
-				LOGGER.error("Exception while waitting for all data of version " + loadingVersion + " to be recieved.", e);
+				LOGGER.error("Exception while waitting for all data of version " + loadingNumber + " to be recieved.", e);
 			}
 			
-			LOGGER.info("All data of version " + loadingVersion + " received. Proceed to the loading of such version.");
-			loadVersion(loadingVersion);
+			LOGGER.info("All data of version " + loadingNumber + " received. Proceed to the loading of such version.");
+			loadVersion("http://graph.version." + loadingNumber);
 			
-			LOGGER.info("Send signal to Benchmark Controller that all data of version " + loadingVersion + " successfully loaded.");
+			LOGGER.info("Send signal to Benchmark Controller that all data of version " + loadingNumber + " successfully loaded.");
 			try {
 				sendToCmdQueue(VirtuosoSystemAdapterConstants.BULK_LOADING_DATA_FINISHED);
 			} catch (IOException e) {
-				LOGGER.error("Exception while sending signal that all data of version " + loadingVersion + " successfully loaded.", e);
+				LOGGER.error("Exception while sending signal that all data of version " + loadingNumber + " successfully loaded.", e);
 			}
-			loadingVersion++;
+			File theDir = new File(datasetFolderName);
+			for (File f : theDir.listFiles()) {
+				f.delete();
+			}
+			loadingNumber++;
 			dataLoadingFinished = lastLoadingPhase;
     	}
     	super.receiveCommand(command, data);
