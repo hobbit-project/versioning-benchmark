ed21d91e45e17990e811f75796144c9eae8446b1
vpapako <vpapako@gmail.com>
2018-03-15 13:24:40 +0200

    move r43ples to its own repository

--- r43ples_system/execute_query.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-MODE=off
-ENDPOINT=http://localhost:9998/r43ples/sparql
-QUERY=$1
-
-curl -H "Accept: application/json" --data "query_rewriting=$MODE&query=$QUERY" $ENDPOINT
--- r43ples_system/server_restart.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-ps -ef | grep r43ples.jar | grep -v grep | awk '{print $2}' | xargs kill
-java -jar r43ples.jar --config conf/r43ples.tdb.conf &
