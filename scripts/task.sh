fbbc6be429b45fa566ede00ec39e77d48d63a927
vpapako <vpapako@gmail.com>
2018-04-19 13:49:40 +0300

    update to develop tags

--- scripts/task.sh
+++ scripts/task.sh
@@ -1,5 +1,5 @@
 
 mvn clean package -U -Dmaven.test.skip=true
-docker build -f docker/versioningtaskgenerator.docker -t git.project-hobbit.eu:4567/papv/versioningtaskgenerator:2.0 .
-docker push git.project-hobbit.eu:4567/papv/versioningtaskgenerator:2.0
+docker build -f docker/versioningtaskgenerator.docker -t git.project-hobbit.eu:4567/papv/versioningtaskgenerator:develop .
+docker push git.project-hobbit.eu:4567/papv/versioningtaskgenerator:develop
 
