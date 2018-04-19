2f8f42557a00557e04c6e652df34793119e45040
vpapako <vpapako@gmail.com>
2018-04-19 15:31:56 +0300

    generalize makefile

--- scripts/virtuoso_system.sh
+++
@@ -1,3 +0,0 @@
-mvn clean package -U -Dmaven.test.skip=true
-docker build -f docker/versioningvirtuososystemadapter.docker -t git.project-hobbit.eu:4567/papv/versioningsystem:develop .
-docker push git.project-hobbit.eu:4567/papv/versioningsystem:develop
