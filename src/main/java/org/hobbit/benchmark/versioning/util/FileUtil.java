8a352716bb0c0c393b350ad41dcdc9ceab622e28
vpapako <vpapako@gmail.com>
2018-07-21 01:10:57 +0300

    add file util class with gz compress method

---
+++ src/main/java/org/hobbit/benchmark/versioning/util/FileUtil.java
@@ -0,0 +1,35 @@
+package org.hobbit.benchmark.versioning.util;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.GZIPOutputStream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FileUtil {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(FileUtil.class);
+	
+	private static void compressGzipFile(String file, String gzipFile) {
+        try {
+            FileInputStream fis = new FileInputStream(file);
+            FileOutputStream fos = new FileOutputStream(gzipFile);
+            GZIPOutputStream gzipOS = new GZIPOutputStream(fos);
+            byte[] buffer = new byte[1024];
+            int len;
+            while((len=fis.read(buffer)) != -1){
+                gzipOS.write(buffer, 0, len);
+            }
+            //close resources
+            gzipOS.close();
+            fos.close();
+            fis.close();
+        } catch (IOException e) {
+            LOGGER.error("An error occured while compressing file.");
+        }
+        
+    }
+
+}
