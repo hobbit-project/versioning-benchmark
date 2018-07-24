afeef2c6cd522f9aca63bc5463d8565c25080db8
vpapako <vpapako@gmail.com>
2018-07-24 14:27:16 +0300

    add methods for compressing/decompressing files

---
+++ src/main/java/org/hobbit/benchmark/versioning/util/CompressUtils.java
@@ -0,0 +1,50 @@
+package org.hobbit.benchmark.versioning.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CompressUtils {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(CompressUtils.class);
+	
+	public static void compressGZIP(File input, File output) throws IOException {
+		long start = System.currentTimeMillis();
+        try (GZIPOutputStream out = new GZIPOutputStream(new FileOutputStream(output))) {
+            try (FileInputStream in = new FileInputStream(input)) {
+                byte[] buffer = new byte[1024];
+                int len;
+                while((len=in.read(buffer)) != -1) {
+                    out.write(buffer, 0, len);
+                }
+            } finally {
+        		LOGGER.info(input.getName() + " compressed successfully (" + input.length() + "-->" + output.length() + ")"
+        				+ "in " + (System.currentTimeMillis() - start) + " ms.");
+            	input.delete();
+            }
+        }
+    }
+	
+	public static void decompressGzip(File input, File output) throws IOException {
+		long start = System.currentTimeMillis();
+        try (GZIPInputStream in = new GZIPInputStream(new FileInputStream(input))) {
+            try (FileOutputStream out = new FileOutputStream(output)) {
+                byte[] buffer = new byte[1024];
+                int len;
+                while((len = in.read(buffer)) != -1) {
+                    out.write(buffer, 0, len);
+                }
+            }
+        } finally {
+        	LOGGER.info(input.getName() + " decompressed successfully (" + input.length() + "-->" + output.length() + ")"
+    				+ "in " + (System.currentTimeMillis() - start) + " ms.");
+        	input.delete();
+        }
+    }
+}
